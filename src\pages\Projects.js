5abb2fb8835c46660e3a6e2e9da93d0251d1dc27
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 17:33:36 +0500

    Add src\pages\Projects.js

---
+++ src\pages\Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <h1>My projects</h1>
+      <p>This is my projects page.</p>
+    </div>
+  );
+}
+
+export default Projects;
